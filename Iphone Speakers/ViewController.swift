b43760b4881ab37ac9635e5e0f1fe6ce327b36c5
admin <admin@dhcp-18-111-91-118.dyn.mit.edu>
2017-03-05 13:54:11 -0500

    hello

--- Iphone Speakers/ViewController.swift
+++ Iphone Speakers/ViewController.swift
@@ -10,16 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var SpeakerButton: UIView!
+    @IBOutlet weak var Toolbar: UIToolbar!
+    var playButton: UIBarButtonItem!
+    var pauseButton: UIBarButtonItem!
+    var RewindButton: UIBarButtonItem!
+
+
+    
+    func playButtonTapped() {
+        var tempitems = Toolbar.items!
+        tempitems[3] = pauseButton
+        Toolbar.items = tempitems;
+        
+    }
+    func pauseButtonTapped() {
+        var tempitems = Toolbar.items!
+        tempitems[3] = playButton
+        Toolbar.items = tempitems;
+    }
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        playButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.play, target: self, action: #selector(ViewController.playButtonTapped))
+        pauseButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.pause, target: self, action: #selector (ViewController.pauseButtonTapped))
+        var tempitems = Toolbar.items!
+        tempitems.insert(playButton, at: 3)
+        Toolbar.items = tempitems;
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+ 
 
 }
 
